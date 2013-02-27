174b589e6de2e9809b385cf30778d5c20d47e205
Anthony <anthonylukach@gmail.com>
2013-02-26 23:33:26 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+
+class TestCommand(test):
+    def run(self):
+        from tests.runtests import runtests
+        runtests()
+
+
+setup(
+    name='vectorformats',
+    version='0.1',
+    description="Vectorformats modified to accept spanned relationships.",
+    long_description=open('README.md').read(),
+    author='Anthony Lukach',
+    author_email='anthonylukach@gmail.com',
+    license='BSD',
+    url='https://github.com/alukach/vectorformats_mod',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Framework :: Django',
+    ],
+    cmdclass={"test": TestCommand},
+)
